e547702d5d78cdb4f65505ada77a6eb4ac46c553
Sai Reddy <sai.reddy@target.com>
2019-04-02 17:47:37 -0500

    deleted App component and created home component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Home from '/home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
