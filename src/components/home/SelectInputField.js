147ee4b6d1272c7cfc0b0274568e6e8c3aa194d6
Sai Reddy <sai.reddy@target.com>
2019-09-28 14:40:11 -0500

    fetching data from rest api

--- src/components/home/SelectInputField.js
+++ src/components/home/SelectInputField.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
+import { Select, InputLabel } from '@material-ui/core';
 
-class SelectInputField extends React.Component{
+class SelectInputField extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             optionValue: [],
@@ -12,39 +12,54 @@ class SelectInputField extends React.Component{
             label: ''
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getValues();
     }
-    getValues(){
-        this.setState({optionValue: this.props.optionValue, label: this.props.label, value:'All'})
+    getValues() {
+        this.setState({ optionValue: this.props.optionValue, label: this.props.label, value: 'All' })
     }
     handleChange = (e) => {
         e.preventDefault()
-        this.setState({value: e.target.value})
+        this.setState({ value: e.target.value })
     }
-    render(){
+    render() {
+        const ITEM_HEIGHT = 48;
+        const ITEM_PADDING_TOP = 8;
+        const MenuProps = {
+            PaperProps: {
+                style: {
+                    maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                    width: 250,
+                },
+            },
+        };
         return (
-            <TextField
-                select
-                label={this.state.label}
-                style={{ color: "#752205", width:'10rem' }}
-                value={this.state.value}
-                onChange={(value) => this.handleChange(value)}
-                InputProps={{
-                    
-                }}
-            >
-                {
-                    this.state.optionValue.map(option => (
-                        <MenuItem key={option.id} value={option.value}>
-                            {option.value}
-                        </MenuItem>
-                    ))
-                }
-            </TextField>
+            <div style={{ float:"left"}}>
+                <InputLabel shrink htmlFor="age-label-placeholder">
+                    {this.state.label}
+                </InputLabel>
+                <Select
+                    multiline
+                    displayEmpty
+                    name="age"
+                    style={{ color: "#752205",
+                width: '10rem', display:"flex"}}
+                    MenuProps={MenuProps}
+                    value={this.state.value}
+                    onChange={(value) => this.handleChange(value)}
+                >
+                    {
+                        this.state.optionValue.map(option => (
+                            <MenuItem key={option.id} value={option.value}>
+                                {option.value}
+                            </MenuItem>
+                        ))
+                    }
+                </Select>
+            </div>
         )
     }
-    
+
 }
 
 
